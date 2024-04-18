a8d7c4ea8289d793dd744b267073be61967b68d0
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-04-18 20:48:49 -0300

    feat: finishing paciente app, tba 'minhas consultas' for paciente and medico

--- paciente/urls.py
+++ paciente/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
+    path(
+        "escolher_horario/<int:id_dados_medicos>/",
+        views.escolher_horario,
+        name="escolher_horario",
+    ),
+    path('agendar_horario/<int:id_data_aberta>/', views.agendar_horario, name="agendar_horario"),
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from medico.models import DadosMedico, Especialidades
+from django.shortcuts import render, redirect
+from medico.models import DadosMedico, Especialidades, DatasAbertas
+from .models import Consulta
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def home(request):
@@ -18,3 +22,38 @@ def home(request):
         return render(
             request, "home.html", {"medicos": medicos, "especialidades": especialidades}
         )
+
+
+def escolher_horario(request, id_dados_medicos):
+    if request.method == "GET":
+        medico = DadosMedico.objects.get(id=id_dados_medicos)
+        datas_abertas = (
+            DatasAbertas.objects.filter(user=medico.user)
+            .filter(data__gte=datetime.now())
+            .filter(agendado=False)
+        )
+        return render(
+            request,
+            "escolher_horario.html",
+            {"medico": medico, "datas_abertas": datas_abertas},
+        )
+
+
+def agendar_horario(request, id_data_aberta):
+    if request.method == "GET":
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+
+        horario_agendado = Consulta(paciente=request.user, data_aberta=data_aberta)
+
+        horario_agendado.save()
+
+        # TODO: Sugestão Tornar atomico
+
+        data_aberta.agendado = True
+        data_aberta.save()
+
+        messages.add_message(
+            request, constants.SUCCESS, "Horário agendado com sucesso."
+        )
+
+        return redirect("/pacientes/minhas_consultas/")
